ca20c4958acc4bb25287e8ec366f9519207db51f
jonlhart88 <jonathanhart@bren.ucsb.edu>
2019-11-24 18:59:39 -0800

    Finish graphs

--- Groundwater_Scenarios_Graphs.R
+++ Groundwater_Scenarios_Graphs.R
@@ -7,37 +7,131 @@ library(here)
 bau_scenarios <- read_csv("Business As Usual Scenarios Data.csv") %>% 
   clean_names()
 
+water_runout <- read_csv("groundwater_runout_years - Sheet1.csv") %>% 
+  clean_names()
 
 ggplot(data = bau_scenarios, aes(x = year)) +
   geom_line(aes(y = high),
-            color = "green",
+            color = "#229954",
             size = 1) +
   geom_line(aes(y = medium),
-            color = "yellow",
+            color = "#F1C40F",
             size = 1) +
   geom_line(aes(y = low),
-            color = "red",
+            color = "#C0392B",
             size = 1) +
   geom_ribbon(aes(ymin = -400,
               ymax = 0),
               fill = "red",
-              alpha = 0.2) +
-  scale_x_continuous(limits = c(2000, 2050),
+              alpha = 0.10) +
+  geom_hline(yintercept = 0,
+             linetype = 2) +
+  scale_x_continuous(limits = c(2000, 2050.5),
                      breaks = seq(2000, 2050, by = 5),
                      expand = c(0,0)) +
   scale_y_continuous(limits = c(-400, 600),
                      breaks = seq(-300, 600, by = 100),
                      expand = c(0,0)) +
   theme_minimal() +
-  geom_text(x = 2015,
-            y = -200, 
-            label = "Groundwater Depleted",
-            size = 4,
-            aes(fontface = 1)) +
   labs(x = "Year",
        y = "Groundwater Flux (10^9 m^3)",
        title = "Groundwater Loss (2000-2050)",
-       subtitle = "Business As Usual Scenario") 
+       subtitle = "Business As Usual Scenario") +
+  geom_text(x = 2012,
+             y = 480,
+             label = "High",
+             angle = -15) +
+  geom_text(x = 2012,
+            y = 285,
+            label = "Medium",
+            angle = -15) +
+  geom_text(x= 2012,
+            y = 120,
+            label = "Low",
+            angle = -15) +
+  annotate("text", 
+           x = 2024, 
+           y = 21.5, 
+           label = "(2022)",
+           size = 3) +
+  annotate("text", 
+           x = 2037, 
+           y = 21.5, 
+           label = "(2035)",
+           size = 3) +
+  annotate("text", 
+           x = 2048.5, 
+           y = -15, 
+           label = "(2049)",
+           size = 3) +
+  theme(axis.text.x = element_text(angle = 90))
+
+ggsave(here::here("Final Figures", "boa_scenarios.png"), width = 10, height = 8)
+
+# Policy Scenarios          
+
+policy_scenarios <- read_csv("Policy Scenarios Data.csv") %>% 
+  clean_names() 
+ 
+           
+ggplot(data = policy_scenarios, aes(x = year)) +
+  geom_line(aes(y = high),
+            color = "#229954",
+            size = 1) +
+  geom_line(aes(y = medium),
+            color = "#F1C40F",
+            size = 1) +
+  geom_line(aes(y = low),
+            color = "#C0392B",
+            size = 1) +
+  geom_ribbon(aes(ymin = -400,
+                  ymax = 0),
+              fill = "red",
+              alpha = 0.10) +
+  geom_hline(yintercept = 0,
+             linetype = 2) +
+  scale_x_continuous(limits = c(2000, 2066),
+                     breaks = seq(2000, 2065, by = 5),
+                     expand = c(0,0)) +
+  scale_y_continuous(limits = c(-400, 600),
+                     breaks = seq(-300, 600, by = 100),
+                     expand = c(0,0)) +
+  theme_minimal() +
+  labs(x = "Year",
+       y = "Groundwater Flux (10^9 m^3)",
+       title = "Groundwater Loss (2000-2050)",
+       subtitle = "Policy Enacted Scenario") +
+  geom_text(x = 2012,
+            y = 500,
+            label = "High",
+            angle = -15) +
+  geom_text(x = 2012,
+            y = 305,
+            label = "Medium",
+            angle = -15) +
+  geom_text(x= 2012,
+            y = 140,
+            label = "Low",
+            angle = -15) +
+  annotate("text", 
+           x = 2028, 
+           y = 23, 
+           label = "(2027)",
+           size = 3) +
+  annotate("text", 
+           x = 2046, 
+           y = 23, 
+           label = "(2045)",
+           size = 3) +
+  annotate("text", 
+           x = 2064.5, 
+           y = 28, 
+           label = "(2064)",
+           size = 3) +
+  theme(axis.text.x = element_text(angle = 90))
+
+ggsave(here::here("Final Figures", "policy_scenarios.png"), width = 10, height = 8)
+
 
 
 
